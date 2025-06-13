42871fb30df6b50fb5ab8ac6d58da442c36f2638
Harish Garg <rishusharma123321@gmail.com>
2025-06-13 18:08:02 +0530

    changes in pagination

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,35 +1,40 @@
 import ProductCard from "@/components/card";
 import Navbar from "@/components/Navbar";
+import PaginationControlled from "@/components/Pagination";
 import fetchproducts from "@/services/productapi";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 
-interface ProductType{
-    id:number;
-    title:string;
-    thumbnail:string;
-    price:number;
-    description:string;
-    category?:string;
+interface ProductType {
+    id: number;
+    title: string;
+    thumbnail: string;
+    price: number;
+    description: string;
+    category?: string;
 }
 
 
 export default async function ProductPage() {
 
-        const products:ProductType[] = await fetchproducts()
+    const products: ProductType[] = await fetchproducts()
 
     return (
         <div>
-                    <Navbar/>
+            <Navbar />
 
-            <Grid container spacing={10} sx={{ padding: 2 }}> 
+            <Grid container spacing={10} sx={{ padding: 2 }}>
 
-                
+
                 {products.map((item) => (
-                    <Grid   key={item.id} >
+                    <Grid key={item.id} >
                         <ProductCard id={item.id} thumbnail={item.thumbnail} price={item.price} title={item.title} description={item.description} category={item.category} />
                     </Grid>
                 ))}
             </Grid>
+            <Box>
+                <PaginationControlled />
+            </Box>
+
         </div>
     )
 }
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,15 +3,9 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import fetchproducts from '@/services/productapi';
-import { useAppDispatch } from '@/app/lib/hooks';
-export default function PaginationControlled() {
-    const [page, setPage] = React.useState(1);
-    const skip = (page - 1) * 10;
-    const dispatch = useAppDispatch()
-    dispatch(setSkip(skip))
-    // const products = fetchproducts(skip)
 
+export default function PaginationControlled() {
+  const [page, setPage] = React.useState(1);
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
@@ -19,7 +13,7 @@ export default function PaginationControlled() {
   return (
     <Stack spacing={2}>
       <Typography>Page: {page}</Typography>
-      <Pagination count={20} page={page} onChange={handleChange} />
+      <Pagination count={10} page={page} onChange={handleChange} />
     </Stack>
   );
 }
